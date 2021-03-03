2cc995d4dce7aaa4abac77760e942d7fe7b5976f
Waqas Hameed <m_waqas_hameed@hotmail.com>
2021-03-03 22:56:20 +0100

    Few changes in layout and author name

--- src/Create.js
+++ src/Create.js
@@ -4,9 +4,9 @@ import { useHistory } from "react-router-dom";
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('Waqas');
+    const [author, setAuthor] = useState('');
     const [isPending, setIsPending] = useState(false);
-    const history = useHistory;
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -44,14 +44,15 @@ const Create = () => {
             onChange={(e) => setBody(e.target.value)}
           ></textarea>
           <label>Blog author:</label>
-          <select
+          <input 
+            type="text" 
+            required 
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-          >
-            <option value="Waqas">Waqas</option>
-            <option value="Khaplu">Khaplu</option>
-            <option value="Daska">Daska</option>
-          </select>
+          />
+        
+          
+          
           { !isPending && <button>Add Blog</button> }
           { isPending && <button>Adding Blog...</button> }
           
